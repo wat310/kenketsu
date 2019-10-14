842b0828c4abff1c7b09c9604319a648075bf28c
wat310 <midtilda_velca@yahoo.co.jp>
2019-10-14 12:59:29 +0900

    header_create
    ヘッダーを完成。
    リンクのパスなどは未実装。

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require rails-ujs
 //= require jquery
+//= require bootstrap-sprockets
 //= require activestorage
 //= require turbolinks
-//= require bootstrap-sprockets
 //= require_tree .
