16cb603f7399c08cbaeeabf8e6a08bffd3718f36
wat310 <midtilda_velca@yahoo.co.jp>
2019-10-24 16:53:17 +0900

    record_validate
    recordのバリデーション 設定

--- app/models/record.rb
+++ app/models/record.rb
@@ -4,5 +4,20 @@ class Record < ApplicationRecord
   enum inspection_method: { four: 0, two: 1, component: 2 }
 
   validates :inspection_method, presence: true
+  validates :donation_day, presence: true
   validates :ALT, presence: true
+  validates :GTP, presence: true
+  validates :TP, presence: true
+  validates :ALB, presence: true
+  validates :AperG, presence: true
+  validates :CHOL, presence: true
+  validates :GA, presence: true
+  validates :RBC, presence: true
+  validates :Hb, presence: true
+  validates :Ht, presence: true
+  validates :MCV, presence: true
+  validates :MCH, presence: true
+  validates :MCHC, presence: true
+  validates :WBC, presence: true
+  validates :PLT, presence: true
 end
