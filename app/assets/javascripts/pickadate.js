b38caf79625bbb3011063e4c368e828d7db55d8d
wat310 <midtilda_velca@yahoo.co.jp>
2019-10-25 11:12:51 +0900

    pickadate_fix
    画面に戻ってきたときに、勝手にカレンダーが表示される問題を修正

--- app/assets/javascripts/pickadate.js
+++ app/assets/javascripts/pickadate.js
@@ -1,5 +1,8 @@
 $(document).on('turbolinks:load', function() {
   $(function() {
+    $(window).on("focus", function () { // これがないと画面に戻った時に勝手にカレンダーが表示される
+      $(document.activeElement).blur();
+    });
     $('#schedule').pickadate({
       min: 0
     });
