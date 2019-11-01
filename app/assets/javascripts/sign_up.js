7268328f9c9f570172f09a601647afccf741c957
wat310 <midtilda_velca@yahoo.co.jp>
2019-11-02 00:52:17 +0900

    password_toggle_create
    パスワードの表示・非表示のtoggleボタンを追加

--- app/assets/javascripts/sign_up.js
+++ app/assets/javascripts/sign_up.js
@@ -1,10 +1,27 @@
 $(document).on('turbolinks:load', function() {
   $(function() {
-    $('#sign_up-toggle').bootstrapToggle({
+    $('#sign_up-toggle').bootstrapToggle({ // 性別選択のtoggleボタン
       on: '男性',
       off: '女性',
       offstyle: 'danger',
       width: '150',
     });
+
+    $('#passcheck').bootstrapToggle({ // パスワードのtoggleボタン
+      on: '表示',
+      off: '隠す',
+      onstyle: 'info',
+      offstyle: 'secondary',
+      width: '100',
+    });
+
+    $('#passcheck').change(function(){
+      if ( $(this).prop('checked') ) {
+          $('.password').attr('type','text');
+      } else {
+          $('.password').attr('type','password');
+      }
+    });
+
   });
 });
