e540c9c024763c26b7429db00c73ed98731c5307
wat310 <midtilda_velca@yahoo.co.jp>
2019-11-14 20:18:31 +0900

    username_validate
    ユーザーのニックネームの重複禁止

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  validates :nickname, presence: true, length: { maximum: 8 }
+  validates :nickname, presence: true, uniqueness: true, length: { maximum: 8 }
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   validates :password, presence: true, length: { minimum: 7, maximum: 15}
   validates :sex, presence: true
